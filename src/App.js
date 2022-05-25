94ee256d7752b82763373492b8f15ac23c8e29b0
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2022-05-25 05:46:41 +0100

    Getting data from API

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import './App.css';
-import './key';
-import { Axios } from 'axios';
-import { useState } from 'react';
+import Pages from './pages/Pages';
 
 function App() {
 
-  const {query, setQuery} = useState("");
-
-  const YOUR_APP_ID ="98ae48c9";
-const YOUR_APP_KEY= "5dadbe216d63560310fe00f37bd98dec";
-
-  var url =`https://api.edamam.com/search?q=chicken&app_id=
-  98ae48c9&app_key=5dadbe216d63560310fe00f37bd98dec$from=0&to=3&
-  calories=591-722&health=alcohol-free`;
 
   return (
     <div className="app">
-      <h1>Food Recipe Plaza</h1>
-      <form className='app__searchForm'>
-      <input className='app__input' type="text" placeholder="enter ingredient"
-      value={query} onChange={(e) => setQuery(e.target.value)} />
-      <input  className='app__submit' type='submit' value='Search' />
-      </form>
+    <Pages/>
     </div>
   );
 }
